674f023b94aac488c99d785d3055864e8ef745da
Ekaterina Gamaleeva <kate_gamaleeva@icloud.com>
2024-06-17 19:14:32 +0300

    new

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        TextEditor textEditor=new TextEditor();
+        TextEditor textEditor = new TextEditor();
         textEditor.append("bomva");
-        textEditor.toStringText();
+        System.out.println( textEditor);
         textEditor.append("vdrdfc");
         textEditor.undo();
-        textEditor.toStringText();
+        System.out.println( textEditor);
         textEditor.append("ergdrg");
         textEditor.undo();
-        textEditor.toStringText();
+        System.out.println( textEditor);
         textEditor.append("g");
         textEditor.undo();
-        textEditor.toStringText();
+        System.out.println( textEditor);
 
     }
-}
+
+    }
+
--- src/main/java/TextEditor.java
+++ src/main/java/TextEditor.java
@@ -4,27 +4,24 @@ import java.util.List;
 public class TextEditor {
     private History history;
     private String text;
-    private List<String> texts;
 
     {
         history= new History();
-        texts=new ArrayList<>();
+
     }
 
-    public void  toStringText(){
-        System.out.print( '\n' +"TextEditor " +
-                    "text='" + text + '\'' +
-                    ", texts="
-    + "\n");
-        texts.stream().forEach(System.out::println);
+    @Override
+    public String toString() {
+        return "TextEditor{" +
+                "text='" + text + '\'' +
+                '}';
     }
 
-    public List<String> getText() {
-        return texts.stream().toList();
+    public String getText() {
+        return text ;
     }
 
     public void append(String text) {
-         texts.add(text);
          this.text=text;
          saveStateMemento();
     }
@@ -37,39 +34,4 @@ public class TextEditor {
         text = history.getMementoSaveNewMomento().getText();
     }
 
-
-
-
-    public void deleteText()
-    {
-        texts=new ArrayList<>();
-    }
-
-    public void deleteTextByIndex(int index){
-        texts.remove(index);
-    }
-    public void deleteTextByObject(String string){
-        texts.remove(string);
-    }
-    public void updateTextByIndex(int index,String s){
-        texts.add(index,s);
-    }
-    public int lastIndexOf(String str){
-        int index=0;
-        return index=lastIndexOf(str);
-    }
-
-    public int indexOf(String str)
-    {
-        int index=0;
-        index=str.indexOf(str);
-        return index;
-    }
-    public List<String> reverse(){
-       ArrayList<String> texts2=new ArrayList<>();
-            for (int i = texts.size() - 1; i >= 0; i--) {
-                texts2.add(texts.get(i));
-        }
-        return texts2;
-    }
 }
